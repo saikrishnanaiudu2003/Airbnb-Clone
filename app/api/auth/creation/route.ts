fac75f8bc3efe240e2690fffd3dcd5a3ac724018
saikrishnanaiudu2003 <131279997+saikrishnanaiudu2003@users.noreply.github.com>
2024-03-17 19:33:27 +0530

    Update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,6 +31,6 @@ export async function GET() {
     });
   }
   return NextResponse.redirect(
-    "https://airbnb-clone-5sxtjzwpg-saikrishnas-projects-1d05a103.vercel.app"
+  "https://airbnb-clone-lovat-eta.vercel.app"
   );
 }
