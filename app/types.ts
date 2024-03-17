d0e595e485840410e91025caecd362b2c459aea4
saikrishnanaiudu2003 <131279997+saikrishnanaiudu2003@users.noreply.github.com>
2024-03-17 17:56:43 +0530

    Update types.ts
    no need

--- app/types.ts
+++ app/types.ts
@@ -1,12 +1,1 @@
-// types.ts
-import { PrismaClient } from '@prisma/client';
 
-interface CustomPrismaClient extends PrismaClient {
-  home: {
-    findFirst: any; // Define the correct type for this method
-    create: any; // Define the correct type for this method
-    // Add other methods if necessary
-  };
-}
-
-export type { CustomPrismaClient };
